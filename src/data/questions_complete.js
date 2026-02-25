8a6a267d9d1d22e1311752fc6ea922f136e58704
fernandadrp <108687606+fernandadrp@users.noreply.github.com>
2026-02-25 20:42:27 +0000

    Add files via upload

---
+++ src/data/questions_complete.js
@@ -0,0 +1,103 @@
+const data = [
+  {
+    category: "HTML",
+    questions: [
+      {
+        question: "Qual tag cria um parágrafo?",
+        options: ["<p>", "<h1>", "<text>", "<ul>"],
+        answer: "<p>",
+        tip: "É uma tag de uma letra apenas",
+      },
+      {
+        question: "Qual atributo adiciona um link para a tag a?",
+        options: ["alt", "href", "src", "link"],
+        answer: "href",
+        tip: "Hyperlink Reference",
+      },
+      {
+        question: "As listas não ordenadas tem a tag de:",
+        options: ["<ol>", "<ul>", "<li>", "<list>"],
+        answer: "<ul>",
+        tip: "Alguma dica",
+      },
+      {
+        question: "Qual atributo deixa o input obrigatório?",
+        options: ["placeholder", "value", "required", "maxlength"],
+        answer: "required",
+        tip: "Outra dica",
+      },
+      {
+        question: "A tag semântica indicada para rodapés é a:",
+        options: ["div", "main", "section", "footer"],
+        answer: "footer",
+        tip: "Outra dica",
+      },
+    ],
+  },
+  {
+    category: "CSS",
+    questions: [
+      {
+        question: "Qual regra altera a cor de um elemento?",
+        options: ["color", "background-color", "font-size", "transition"],
+        answer: "color",
+        tip: "Cor em inglês",
+      },
+      {
+        question: "Para aumentar a fonte de um elemento utilizamos:",
+        options: ["font", "text-transform", "font-size", "hover"],
+        answer: "font-size",
+        tip: "Outra dica",
+      },
+      {
+        question: "A posição que deixa um elemento fixo é a:",
+        options: ["static", "absolute", "fixed", "relative"],
+        answer: "fixed",
+        tip: "Outra dica",
+      },
+    ],
+  },
+  {
+    category: "JavaScript",
+    questions: [
+      {
+        question: "O que é Vanilla JavaScript?",
+        options: [
+          "JavaScript puro",
+          "Uma biblioteca JavaScript",
+          "Um framework JavaScript",
+          "Um compilador de JavaScript",
+        ],
+        answer: "JavaScript puro",
+        tip: "Outra dica",
+      },
+      {
+        question: "Com qual instrução declaramos uma constante em JavaScript?",
+        options: ["const", "let", "var", "define"],
+        answer: "const",
+        tip: "Outra dica",
+      },
+      {
+        question: "Qual dos tipos de dado a seguir não existe em JavaScript?",
+        options: ["string", "number", "boolean", "float"],
+        answer: "float",
+        tip: "Outra dica",
+      },
+      {
+        question: "Qual dos métodos a seguir seleciona um elemento?",
+        options: ["querySelector", "parseInt", "sort", "reduce"],
+        answer: "querySelector",
+        tip: "Outra dica",
+      },
+      {
+        question:
+          "Qual destas propriedades da a quantidade de elementos de um array?",
+        options: ["qty", "length", "items", "index"],
+        answer: "length",
+        tip: "Outra dica",
+      },
+    ],
+  },
+];
+
+export default data;
